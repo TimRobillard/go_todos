bce46bd9bae62b453510e2eccaab97f73428816a
Tim Robillard <robillt@algonquincollege.com>
2024-02-25 10:21:38 -0500

    refactor 2

--- api/main.go
+++ api/main.go
@@ -1,12 +1,9 @@
 package api
 
 import (
-	"fmt"
 	"html/template"
 	"io"
-	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/TimRobillard/todo_go/store"
 	"github.com/labstack/echo/v4"
@@ -42,42 +39,7 @@ func Register(e *echo.Echo, pg *store.PostgresStore) error {
 	e.Renderer = t
 	e.Static("/dist", "dist")
 
-	e.GET("/", func(c echo.Context) error {
-		todos, err := pg.GetAllTodos()
-		if err != nil {
-			fmt.Printf(err.Error())
-			return c.String(http.StatusNotFound, "Something went wrong")
-		}
-
-		return c.Render(http.StatusOK, "index", todos)
-	})
-
-	e.POST("/todo", func(c echo.Context) error {
-		title := c.FormValue("todo")
-		todo, err := pg.CreateToDo(title)
-
-		if err != nil {
-			return c.String(http.StatusNotFound, err.Error())
-		}
-
-		return c.Render(http.StatusOK, "todo", todo)
-	})
-
-	e.PUT("/todo/:id", func(c echo.Context) error {
-		_id := c.Param("id")
-		id, err := strconv.Atoi(_id)
-
-		if err != nil {
-			return err
-		}
-
-		err = pg.ToggleTodo(id)
-		if err != nil {
-			return c.String(http.StatusNotFound, err.Error())
-		}
-
-		return c.NoContent(http.StatusOK)
-	})
+	RegisterTodoRoutes(e, pg)
 
 	return nil
 }
--- api/todo.go
+++ api/todo.go
@@ -1,1 +1,51 @@
 package api
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/TimRobillard/todo_go/store"
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterTodoRoutes(e *echo.Echo, pg *store.PostgresStore) error {
+	e.GET("/", func(c echo.Context) error {
+		todos, err := pg.GetAllTodos()
+		if err != nil {
+			fmt.Printf(err.Error())
+			return c.String(http.StatusNotFound, "Something went wrong")
+		}
+
+		return c.Render(http.StatusOK, "index", todos)
+	})
+
+	e.POST("/todo", func(c echo.Context) error {
+		title := c.FormValue("todo")
+		todo, err := pg.CreateToDo(title)
+
+		if err != nil {
+			return c.String(http.StatusNotFound, err.Error())
+		}
+
+		return c.Render(http.StatusOK, "todo", todo)
+	})
+
+	e.PUT("/todo/:id", func(c echo.Context) error {
+		_id := c.Param("id")
+		id, err := strconv.Atoi(_id)
+
+		if err != nil {
+			return err
+		}
+
+		err = pg.ToggleTodo(id)
+		if err != nil {
+			return c.String(http.StatusNotFound, err.Error())
+		}
+
+		return c.NoContent(http.StatusOK)
+	})
+
+	return nil
+}
