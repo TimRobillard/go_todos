0c9b8cd4958ec7e7c6c467ae75b2d06d7510902e
Tim Robillard <robillt@algonquincollege.com>
2024-02-29 20:33:50 -0500

    use env vars for psql

--- store/main.go
+++ store/main.go
@@ -14,8 +14,8 @@ type PostgresStore struct {
 
 func NewPostgresStore() (*PostgresStore, error) {
 	user := os.Getenv("PG_USER")
-	dbname := os.Getenv("PG_USER")
-	pw := os.Getenv("PG_USER")
+	dbname := os.Getenv("PG_DBNAME")
+	pw := os.Getenv("PG_PASSDWORD")
 	connStr := fmt.Sprintf("user=%s dbname=%s password=%s sslmode=disable", user, dbname, pw)
 	if db, err := sql.Open("postgres", connStr); err != nil {
 		return nil, err
